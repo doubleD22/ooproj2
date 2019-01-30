3bc31095b77013317b9175a7e0b21d7996451dcc
BillGatesIsAThief <danielman1803@gmail.com>
2019-01-30 22:02:32 +0200

    Revert "/2"
    This reverts commit 460a72223617e3ecbd599bdabb031f0db68fe362.

--- oop2/Canvas.h
+++ oop2/Canvas.h
@@ -4,17 +4,17 @@
 class Canvas : public Shape
 {
 private:
-	Shape ** m_shapes;	 //creating an array of pointers of shapes
-	int m_length;	   // the lenght of the array = how mant shapes there are
-	Canvas(const Canvas& c);  //CC
+	Shape ** m_shapes; //creating 
+	int m_length;
+	Canvas(const Canvas& c);
 public:
 	Canvas(const Point& p , Color c); //constructor
-	~Canvas(); //destructor 
+	~Canvas();
 	void draw();
 	void scale(const int& factor);
 	double area();
-	void insert_shape(Shape* shape); // inserting a pointer of shape in the array of pointers
+	void insert_shape(Shape* shape);
 	//Canvas& operator=(const Canvas& c);
-	static int m_counter; //counting how many shape of Canvas we have
+	static int m_counter;
 };
 
