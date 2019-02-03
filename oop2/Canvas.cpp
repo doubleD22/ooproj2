2102e2912464b382ff23872fb233e28eb6ad4988
BillGatesIsAThief <danielman1803@gmail.com>
2019-02-03 14:26:22 +0200

    1

--- oop2/Canvas.cpp
+++ oop2/Canvas.cpp
@@ -2,11 +2,23 @@
 
 int Canvas::m_counter(0);
 
-Canvas::Canvas(const Point& p = Point(0,0) , Color c = Color::white) : Shape(p,c)
+
+Canvas::Canvas(const Point & p, Color c = Color::white, const int & maxsize = 0) : Shape(p, c)
 {
-	m_shapes = nullptr;
+	m_maxsize = maxsize;
 	m_length = 0;
 	m_counter++;
+	if (m_maxsize)
+	{
+		m_shapes = new Shape*[m_maxsize];
+		if (!m_shapes)
+		{
+			cout << "error!" << endl;
+			exit(1);
+		}
+	}
+	else
+		m_shapes = nullptr;
 }
 
 Canvas::~Canvas()
@@ -31,7 +43,7 @@ void Canvas::draw() //printing
 
 void Canvas::scale(const int & factor)
 {
-	for (int i = 0; i < m_length; i++)
+	for (int i = 0; i < m_maxsize; i++)
 		m_shapes[i]->scale(factor);
 }
 
@@ -39,29 +51,23 @@ double Canvas::area()
 {
 	double temp = 0;
 	for (int i = 0; i < m_length; i++)
-		temp += (*m_shapes)[i].area();
+		temp += m_shapes[i]->area();
 	return temp;
 }
 
-void Canvas::insert_shape(Shape * shape)
+bool Canvas::insert_shape(Shape * shape)
 {
-	Shape** temp = new Shape*[m_length + 1];
-	for (int i = 0; i < m_length; i++)
-		temp[i] = m_shapes[i];
-	temp[m_length] = shape;
-	if(m_shapes)
-		delete[] m_shapes;
-	m_shapes = temp;
-	m_length++;
+	if (m_length = m_maxsize)
+	{
+		cout << "error!, the canvas is full." << endl;
+		return false;
+	}
+	else
+	{
+		m_length++;
+		m_shapes[m_length - 1] = shape;
+		return true;
+	}
 }
 
-//Canvas & Canvas::operator=(const Canvas & c)
-//{
-//	m_length = c.m_length;
-//	delete[] m_shapes;
-//	Shape** m_shapes = new Shape*[m_length];
-//	for (int i = 0; i < m_length; i++)
-//		m_shapes[i] = c.m_shapes[i];
-//	return *this;
-//}
 
--- oop2/Canvas.h
+++ oop2/Canvas.h
@@ -5,15 +5,16 @@ class Canvas : public Shape
 {
 private:
 	Shape ** m_shapes; //creating 
-	int m_length;
+	int m_maxsize;	//maximum number of shapes in this canvas
+	int m_length;	//length of the polymorphic array.
 	Canvas(const Canvas& c);
 public:
-	Canvas(const Point& p , Color c); //constructor
+	Canvas(const Point& p , Color c, const int& length); //constructor
 	~Canvas();
 	void draw();
 	void scale(const int& factor);
 	double area();
-	void insert_shape(Shape* shape);
+	bool insert_shape(Shape* shape);
 	//Canvas& operator=(const Canvas& c);
 	static int m_counter;
 };
